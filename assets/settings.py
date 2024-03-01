cbf56889c0611be56118ede85c9d6988c0807907
lucacdev <lucacolombo2003.it@gmail.com>
2024-02-29 22:12:01 -0300

    added sprites

---
+++ assets/settings.py
@@ -0,0 +1,28 @@
+import pygame as pg
+
+vec = pg.math.Vector2
+
+FPS = 60
+FIELD_COLOUR = (48, 39, 32)
+
+SPRITE_DIR_PATH = 'assets/sprites'
+
+ANIM_TIME_INTERVAL = 150 # ms
+FAST_ANIM_TIME_INTERVAL = 15 # ms
+
+TILE_SIZE = 35
+FIELD_SIZE = FIELD_W, FIELD_H = 10, 20
+FIELD_RES = FIELD_W * TILE_SIZE, FIELD_H * TILE_SIZE
+
+INIT_POS_OFFSET = vec(FIELD_W // 2 - 1, 0)
+MOVE_DIRECTIONS = {'left': vec(-1, 0), 'right': vec(1, 0), 'down': vec(0, 1)}
+
+TETROMINOES = {
+    'O' : [(0, 0), (0, -1), (1, 0), (1, -1)],
+    'I' : [(0, 0), (0, 1), (0, -1), (0, -2)],
+    'S' : [(0, 0), (-1, 0), (0, -1), (1, -1)],
+    'Z' : [(0, 0), (1, 0), (0, -1), (-1, -1)],
+    'L' : [(0, 0), (1, 0), (0, -1), (0, -2)],
+    'J' : [(0, 0), (-1, 0), (0, -1), (0, -2)],
+    'T' : [(0, 0), (-1, 0), (1, 0), (0, -1)]
+}
