caf6f38f05fa5bebf5d6fff0bb2e6c7866e69a61
lucacdev <lucacolombo2003.it@gmail.com>
2024-03-02 13:30:14 -0300

    restart implemented

--- .history/assets/tetris_20240302142434.py
+++ .history/assets/tetris_20240302142434.py
@@ -36,7 +36,7 @@ class Tetris:
         return [[0 for x in range(FIELD_W)] for y in range(FIELD_H)]
     
     def is_game_over(self):
-        if self.tetromino.blocks[0].pos.y == INIT_POS_OFFSET[1]:
+        if self.tetrominoblocks[0].pos.y == INIT_POS_OFFSET[1]:
             pg.time.wait(300)
             return True
 
